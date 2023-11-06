68b60eaa5b50c0e90fe3a6047dc9fbbf838dfbef
roma-developer <ryzikov16@gmail.com>
2023-11-06 20:33:18 +0700

    version 0.1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,62 @@
-"use strict";
+const cardBox = document.querySelector('.card-box')
+let countOpenCard = 0
+let allowedOpenCard = true
 
-let cards = document.querySelectorAll('.card')
+const cardsArr = [
+    'clover',
+    'heart',
+    'potted-cactus',
+    'fox',
+    'chess',
+    'fighting',
+    'octopus'
+] 
 
-
-for (let card of cards){
-    card.addEventListener('click', function(){
-        card.classList.toggle('card__inverted')
+let htmlCards = ''
+function CardHtmlGeneration (arr){
+    let sumArr = arr.concat(arr)
+    sumArr.sort(() => Math.random() - 0.5)
+    sumArr.forEach((elem)=>{
+        console.log(elem)
+        htmlCards += '<div class="card" data-img="' + elem + '"><div class="card__drawing card__drawing--' + elem + '" style="background-image: url(../img/' + elem + '.svg);"></div><div class="card__card-shirt"></div></div>'
     })
+}
+
+CardHtmlGeneration (cardsArr)
+cardBox.innerHTML = htmlCards
+
+document.querySelectorAll('.card').forEach((card) => {
+    card.addEventListener('click', function(){
+        if (allowedOpenCard && !card.classList.contains('card__inverted')  && !card.classList.contains('card__filter')){
+            card.classList.add('card__inverted')
+            countOpenCard++
+            if (countOpenCard == 2){
+                allowedOpenCard = false
+                ChekCard()
+            }
+        }
+    })
+})
+
+function ChekCard(){
+    let openCards = document.querySelectorAll('.card__inverted')
+    setTimeout(() => {
+        if (openCards[0].dataset.img == openCards[1].dataset.img){
+            DeactivateCard(openCards, true)
+        }
+        else{
+            DeactivateCard(openCards, false)
+        }
+    }, 600)
+}
+
+function DeactivateCard(elements , filter){
+    elements.forEach((element) =>{
+        element.classList.remove('card__inverted')
+        if (filter){
+            element.classList.add('card__filter')
+        }
+    })
+    allowedOpenCard = true
+    countOpenCard = 0
 }
